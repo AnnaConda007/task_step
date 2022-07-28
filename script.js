2b243de2cff257b0688ca4ca62346ca0402c8c84
AnnaConda007 <106409048+AnnaConda007@users.noreply.github.com>
2022-07-28 19:58:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let body = document.querySelector(".task_body")
+let num = +prompt()
+let size_step=100/num        /*заменить на значение переменной*/
+var root= document.querySelector(":root");
+let step
+let shift_right = 0
+
+
+let button= document.querySelector("button");
+button.addEventListener("click",  create_step )
+function create_step(){   
+let k=0
+    if(k < num){ 
+        k++
+        step = document.createElement("div")
+        body.append(step)
+         step.style.width= size_step + "%"
+         step.style.height=size_step + "%"
+         step.style.backgroundColor="blue"
+         step.style.position= "absolute"
+         step.style.left=  shift_right + "%"
+         step.style.bottom= shift_right + "%"
+         shift_right= shift_right + size_step
+      }}
+
+      input_form=0     /* ?????? input_form ранее нигде не объявлено, но работает , если удалит 0 - не работает */
+let k_id_create_input=0
+let button_input =  document.getElementById("input_button")   
+button_input.addEventListener("click", create_input)
+function create_input(){
+    input_form = document.createElement("form")    
+    input_form.id="form";
+    body.append(input_form);
+    let new_input=document.createElement("input");
+    input_form.append(new_input);
+    new_input.id=k_id_create_input
+    k_id_create_input++
+    let send_button = document.createElement("button")
+body.append(send_button)
+send_button.id = "send"
+send_button.style.position="absolute"
+send_button.style.bottom="20px"
+send_button.setAttribute('tupe', 'submit');
+send_button.setAttribute("label for='send'", "send")
+}
+ 
+
+let form = document.getElementById("form")
+form.addEventListener("submit", function(){
+    let value_input=document.getElementById("new_input").value
+   alert(value_input)
+   })
+    
+   
