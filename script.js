adaa87b153d24c99e03328269cb19a8f67bf483d
AnnaConda007 <annahrulkova@yandex.ru>
2022-07-28 22:32:59 +0300

    получили значение поля ввода

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let body = document.querySelector(".task_body")
-let num = +prompt()
+let num = 5/*+prompt()*/
 let size_step=100/num        /*заменить на значение переменной*/
 var root= document.querySelector(":root");
 let step
@@ -24,31 +24,38 @@ let k=0
       }}
 
       input_form=0     /* ?????? input_form ранее нигде не объявлено, но работает , если удалит 0 - не работает */
-let k_id_create_input=0
+
+      let k_id_create_input=0
 let button_input =  document.getElementById("input_button")   
 button_input.addEventListener("click", create_input)
 function create_input(){
     input_form = document.createElement("form")    
-    input_form.id="form";
+    input_form.id="form"
     body.append(input_form);
     let new_input=document.createElement("input");
     input_form.append(new_input);
-    new_input.id=k_id_create_input
+    new_input.id="new_input"
+    new_input.setAttribute('type', 'text');
+    new_input.setAttribute('value', 'tedfxt');
     k_id_create_input++
-    let send_button = document.createElement("button")
-body.append(send_button)
+    let send_button = document.createElement("input")
+    input_form.append(send_button)
 send_button.id = "send"
 send_button.style.position="absolute"
 send_button.style.bottom="20px"
-send_button.setAttribute('tupe', 'submit');
-send_button.setAttribute("label for='send'", "send")
+send_button.setAttribute('type', 'submit');
+let form=document.getElementById("form")
+form.addEventListener("submit", function(){
+    let value_input=document.getElementById("new_input").value
+    let all_value=[]
+    all_value.push(value_input)
+   alert(value_input)
+
+})
+
 }
  
 
-let form = document.getElementById("form")
-form.addEventListener("submit", function(){
-    let value_input=document.getElementById("new_input").value
-   alert(value_input)
-   })
-    
-   
+
+ 
+
