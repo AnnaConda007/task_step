d80cd13d889a828d0591a00a1dc9614675db9562
AnnaConda007 <annahrulkova@yandex.ru>
2022-07-30 22:58:32 +0300

    промежуточное изменение

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
 let  arr=[1,2,]
-let body = document.querySelector(".task_body")
+let task_body = document.querySelector(".task_body")
+let body = document.querySelector("body")
 let num = 5/*+prompt()*/
 let size_step=100/num        /*заменить на значение переменной*/
 var root= document.querySelector(":root");
 let step
 let shift_right = 0
 let k_of_step=0
+let new_input
 
 let button= document.querySelector("button");
 button.addEventListener("click",  create_step )
@@ -14,7 +16,7 @@ function create_step(){
     if(k_of_step < num){ 
         k_of_step++
         step = document.createElement("div")
-        body.append(step)
+        task_body.append(step)
          step.style.width= size_step + "%"
          step.style.height=size_step + "%"
          step.style.backgroundColor="blue"
@@ -23,48 +25,46 @@ function create_step(){
          step.style.bottom= shift_right + "%"
          shift_right= shift_right + size_step
       }}
-
-
-      input_form=0     /* ?????? input_form ранее нигде не объявлено, но работает , если удалит 0 - не работает */
-
-         
-      let value_input=5
-      let new_input
 let button_input =  document.getElementById("input_button")   
-button_input.addEventListener("click", create_input)
-function create_input(){
-    input_form = document.createElement("form")    
+button_input.addEventListener("click", create_form)
+function create_form(){
+    this.remove()
+    let input_form = document.createElement("form")    
     input_form.id="form"
-    body.append(input_form);
+    task_body.append(input_form);
      new_input=document.createElement("input");
     input_form.append(new_input);
     new_input.id="new_input"
     new_input.setAttribute('type', 'text');
-    new_input.setAttribute;
     let send_button = document.createElement("input")
     input_form.append(send_button)
     send_button.id = "send"
     send_button.style.position="absolute"
     send_button.style.bottom="20px"
     send_button.setAttribute('type', 'submit');
-
     let form=document.getElementById("form")
     form.addEventListener("submit", function(evt) {  
-    value_input=document.getElementById("new_input").value
+    let value_input=document.getElementById("new_input").value
     arr.push(value_input)
-    
     evt.preventDefault()
-    
-    
     })
-
+    create_arr_button ()
 }
+
+function create_arr_button (){
+let create_arr_button = document.createElement("button")
+task_body.append(create_arr_button)
+create_arr_button.addEventListener("click", function(){
+create_arr_area()
+
+})}
+
+
+function create_arr_area(){
+    let arr_area=document.createElement("div")
+    body.append(arr_area)
+    arr_area.style.width="50%"
+    arr_area.style.height="50%"
+    arr_area.style.backgroundColor="red"
  
- 
-let c = document.getElementById("vv")
-
-
-c.addEventListener("click", function(){
-
-    alert(arr)
-})
+}
