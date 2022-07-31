534ea7d50dc5c8697c6181065d5214160e94b319
AnnaConda007 <annahrulkova@yandex.ru>
2022-07-31 21:19:13 +0300

    добавлен checkbox

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ var root= document.querySelector(":root");
 let step
 let shift_right = 0
 let k_of_step=0
-let new_input
+let primary_input
 let k_arr=0
 let ol 
-let input_form
+let primary_form
+let  button_ready
 /*
 let button= document.querySelector("button");
 button.addEventListener("click",  create_step )
@@ -30,61 +31,63 @@ function create_step(){
       }}
 */
 
-let button_input =  document.getElementById("input_button")   
-button_input.addEventListener("click", create_form)
-function create_form(){
-    this.remove()
-      input_form = document.createElement("form")    
-    input_form.id="form"
-    body.append(input_form);
-     new_input=document.createElement("input");
-    input_form.append(new_input);
-    new_input.id="new_input"
-    new_input.setAttribute('type', 'text');
-    let send_button = document.createElement("input")
-    input_form.append(send_button)
-    send_button.id = "send"
-    send_button.setAttribute('type', 'submit');
-    send_button.setAttribute("value", "следующий шаг" )
-    let form=document.getElementById("form")
-    form.addEventListener("submit", function(evt) {  
-    let value_input=document.getElementById("new_input").value
-    arr.push(value_input)
+let start_button =  document.getElementById("input_button")   
+start_button.addEventListener("click", start_form)
+
+function start_form(){
+    start_button.remove()
+      primary_form = document.createElement("form")    
+      primary_form.id="form"
+    body.append(primary_form);
+    primary_input=document.createElement("input");
+     primary_form.append(primary_input);
+     primary_input.id="primary_input"
+    primary_input.setAttribute('type', 'text');
+    let button__send_value_in_arr = document.createElement("input")
+    primary_form.append(button__send_value_in_arr)
+    button__send_value_in_arr.id = "send"
+    button__send_value_in_arr.setAttribute('type', 'submit');
+    button__send_value_in_arr.setAttribute("value", "следующий шаг" )
+    primary_form.addEventListener("submit", function(evt) {  
+    let value_of_primary_input=document.getElementById("primary_input").value
+    arr.push(value_of_primary_input)
     evt.preventDefault()
-    let li=document.createElement("li")
-    ol.append(li)
-    li.innerHTML=arr[k_arr]
+    arr_append_value_primary_form()
     k_arr++
     })
-    arr_area()
-    
+    create_list_of_arr_area()
 }
+function arr_append_value_primary_form(){
+    let li=document.createElement("input")
+    li.setAttribute('type', 'checkbox');
+    li.id="li";
+    let label=document.createElement("label")
+    ol.append(label)
+    label.innerHTML="value"
+    label.setAttribute("for","li")
+    ol.append(li)
+    li.innerHTML=arr[k_arr]
+ }
 
-let step_button
-function arr_area(){
+function create_list_of_arr_area(){
      arr_area =document.createElement("div")
      body.append(arr_area)
-       ol = document.createElement("ol")
+       ol = document.createElement("form")
      arr_area.append(ol)
      arr_area.id="arr_area"
+     button_ready=document.createElement("button") 
+     button_ready.innerHTML="готово"
+        button_ready.id="step_button"
+        arr_area.append(button_ready)
     
-
-     
-        step_button=document.createElement("button") 
-        step_button.innerHTML="готово"
-        step_button.id="step_button"
-        arr_area.append(step_button)
-    
-    step_button.addEventListener("click", function(){
+        button_ready.addEventListener("click", function(){
         let step_area=document.createElement("div")
     body.append(step_area)
     step_area.id ="task_body" 
-    alert("cvghjk")
-    input_form.remove()
+    primary_form.remove()
     })
    
 }
 
 
 
- 
