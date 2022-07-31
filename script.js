f3268fa37f2a065992d89618c6f013302f6132a8
AnnaConda007 <annahrulkova@yandex.ru>
2022-07-31 19:32:48 +0300

    изменена верстка, отображаются основные элементы

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ let k_of_step=0
 let new_input
 let k_arr=0
 let ol 
-
+let input_form
+/*
 let button= document.querySelector("button");
 button.addEventListener("click",  create_step )
 function create_step(){  
@@ -22,18 +23,18 @@ function create_step(){
          step.style.width= size_step + "%"
          step.style.height=size_step + "%"
          step.style.backgroundColor="blue"
-         step.style.position= "absolute                       /*//////"
+         step.style.position= "absolute                   
          step.style.left=  shift_right + "%"
          step.style.bottom= shift_right + "%"
          shift_right= shift_right + size_step
       }}
-
+*/
 
 let button_input =  document.getElementById("input_button")   
 button_input.addEventListener("click", create_form)
 function create_form(){
     this.remove()
-    let input_form = document.createElement("form")    
+      input_form = document.createElement("form")    
     input_form.id="form"
     body.append(input_form);
      new_input=document.createElement("input");
@@ -44,9 +45,8 @@ function create_form(){
     input_form.append(send_button)
     send_button.id = "send"
     send_button.setAttribute('type', 'submit');
+    send_button.setAttribute("value", "следующий шаг" )
     let form=document.getElementById("form")
-    
-
     form.addEventListener("submit", function(evt) {  
     let value_input=document.getElementById("new_input").value
     arr.push(value_input)
@@ -57,6 +57,7 @@ function create_form(){
     k_arr++
     })
     arr_area()
+    
 }
 
 let step_button
@@ -66,15 +67,22 @@ function arr_area(){
        ol = document.createElement("ol")
      arr_area.append(ol)
      arr_area.id="arr_area"
-    step_button=document.createElement("button")
-    arr_area.append(step_button)
     
-step_button.addEventListener("click", function(){
-    let step_area=document.createElement("div")
-body.append(step_area)
-step_area.id ="task_body" 
-})
+
+     
+        step_button=document.createElement("button") 
+        step_button.innerHTML="готово"
+        step_button.id="step_button"
+        arr_area.append(step_button)
     
+    step_button.addEventListener("click", function(){
+        let step_area=document.createElement("div")
+    body.append(step_area)
+    step_area.id ="task_body" 
+    alert("cvghjk")
+    input_form.remove()
+    })
+   
 }
 
 
